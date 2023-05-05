1cd2dfc6ec884976297fc9d85cb3fba48ae36799
有馬玄規 <haruki@arimagenkinoMacBook-Air.local>
2023-05-05 17:22:59 +0900

    ABOUT-US作成

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,9 +6,10 @@ import "slick-carousel/slick/slick-theme.css";
 import bike from "../../images/bike.jpg";
 import bike2 from "../../images/bike2.jpg";
 import bike3 from "../../images/bike3.jpg";
-
+import shop from "../../images/shop.jpg";
 const Home = () => {
   const settings = {
+    //スライドアニメーションの設定
     fade: true, // fedeオン
     speed: 1500, // 画像切り替えにかかる時間（ミリ秒）
     autoplaySpeed: 3000, // 自動スライド切り替え速度
@@ -18,17 +19,31 @@ const Home = () => {
     slidesToScroll: 1, // スライドする数
     infinite: true, // 無限リピート オン・オフ
   };
-  const images = [bike, bike2, bike3];
+  const images = [bike, bike2, bike3]; //スライダーで表示する画像の配列
+
   return (
-    <Slider {...settings}>
-      {images.map((img) => {
-        return (
-          <div className="bike">
-            <img src={img} alt="bike" />
-          </div>
-        );
-      })}
-    </Slider>
+    <>
+      <Slider {...settings}>
+        {images.map((img) => {
+          return (
+            <div>
+              <img src={img} className="sliderBike" />
+            </div>
+          );
+        })}
+      </Slider>
+      <h2 className="ABOUT-US">ABOUT-US</h2>
+      <div className="flex">
+        <img src={shop} className="shop" />
+        <div className="flexFlow">
+          <p className="aboutusTitle">Swell Moter Cycle</p>
+          <p className="aboutuIntroductionText">
+            佐賀県鳥栖市にあるディーラー。バイクや車の販売や整備を主に行なっており、経験、知識の豊富なオーナーが経営しています。アメ車やドイツ車喉も所有しており、お好きな方はぜひ一度訪れてください。
+          </p>
+        </div>
+      </div>
+      ;
+    </>
   );
 };
 export default Home;
