fd6bac1560d215fb3724a5ed5b719cdce1072168
有馬玄規 <haruki@arimagenkinoMacBook-Air.local>
2023-07-29 15:59:39 +0900

    componentsからpagesにファイルを移した

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBell } from '@fortawesome/free-solid-svg-icons'
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <nav>
-        <Link to="/home">HOME</Link>
-        <Link to="/gallery">GALLERY</Link>
-        <Link to="/blog">BLOG</Link>
-        <Link to="/access">ACCESS</Link>
-        <FontAwesomeIcon icon={faBell} />
-    </nav>
-  )
-}
+    <>
+      <div className="navbarLog">
+        <h1 className="log">Swell Moter Cycle</h1>
 
-export default Navbar
+        <nav>
+          <Link to="/home">HOME</Link>
+          <Link to="/gallery">GALLERY</Link>
+          <Link to="/blog">BLOG</Link>
+          <Link to="/access">ACCESS</Link>
+          <FontAwesomeIcon icon={faBell} />
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
