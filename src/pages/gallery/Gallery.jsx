da20601ece929b8f2d1de8b3860080ba621aae05
有馬玄規 <haruki@arimagenkinoMacBook-Air.local>
2023-08-14 13:16:57 +0900

    galleryページのfirebase連携

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,15 +1,15 @@
 import "./Gallery.css";
 import getFirebase from "../../firebase/firebase";
 import ReactPaginate from "react-paginate";
-import React, { useEffect, useState } from "react";
-import { listAll } from "firebase/compat/storage";
+import React, {useEffect, useState} from "react";
+import "firebase/compat/storage";
 
 const BookList = (props) => {
     return (
         <ul className="imageList">
             {props.currentBooks &&
                 props.currentBooks.map((book) => {
-                    return<li><img src={book.image_url} className="bookListImage"/></li>
+                    return<li key={book.image_url}><img src={book.image_url} className="bookListImage"/></li>
                 })}
         </ul>
     );
@@ -17,29 +17,29 @@ const BookList = (props) => {
 
 const Gallery = () => {
     const [books, setBooks] = useState([]);
+    const storage = getFirebase().storage();
+    const storageRef = storage.ref('gallery/image');
+
     useEffect(() => {
-        const fetchImage = async () => {
-            const storage = getFirebase().storage();
-            const storageRef = storage.ref('gallery/image');
-            let books = [];
-        
-            await storageRef.listAll()
-                .then((res) => {
-                    res.items.forEach(async (item) => {
-                        await storageRef.child(item.name).getDownloadURL().then((url) => {
-                            books.push({
-                                'image_url': url
-                            });
-                        });
-                    });
-                }).catch((error) => {
-            });
-            console.log(books);
-            setBooks(books);
+        const fetchURLs = async () => {
+            const urls = await getBookURLs(storageRef);
+            setBooks(urls.map((url) => {
+                return {'image_url': url}
+            }));
         };
-        fetchImage();
+        fetchURLs();
     }, [])
-    
+
+    const getBookURLs = async (storageRef) => {
+        const all = await storageRef.listAll();
+        const itemNames = all.items.map((item) => item.name);
+        return await Promise.all(itemNames.map(async (name) => await getBookURL(storageRef, name)));
+    }
+
+     const getBookURL = async (storageRef, itemName) => {
+        return await storageRef.child(itemName).getDownloadURL()
+    }
+
     const [startOffset, setStartOffset] = useState(0);
     const perPage = 8;
     const endStartOffset = startOffset + perPage;
@@ -50,6 +50,7 @@ const Gallery = () => {
         const newOffset = (event.selected * perPage) % books.length;
         setStartOffset(newOffset);
     };
+
     return (
         <>
             <BookList currentBooks={currentBooks} />
