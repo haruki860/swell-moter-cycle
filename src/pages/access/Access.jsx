682a99631ff040271a67f66356e15e9f6d6f0019
有馬玄規 <haruki@arimagenkinoMacBook-Air.local>
2023-08-22 14:47:35 +0900

    アクセスページのレスポンシブ対応

--- src/pages/access/Access.jsx
+++ src/pages/access/Access.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Access.css";
-import { GoogleMap, LoadScript} from "@react-google-maps/api";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 const Access = () => {
   const container = {
-    width: "50%",
-    height: "500px",
+    width: "60vw",
+    height: "60vh",
   };
 
   const position = {
@@ -16,15 +16,20 @@ const Access = () => {
   const MAP_API_KEY = process.env.REACT_APP_MAP_API_KEY;
   return (
     <>
-      <h2 className="shop">店舗住所&emsp;/&emsp;営業時間</h2>
       <div className="information">
-        <div className="mailAddress">
-          <div className="mail">841-0041</div>
-          <div className="address">佐賀県鳥栖市高田町73-4</div>
+        <div className="shopTitle">
+          <div className="shopAddress">店舗住所</div>
+          <div className="shopHours">営業時間</div>
         </div>
-        <div className="businessHours">
-          <div className="weekday">月〜金&emsp;10:00〜20:00</div>
-          <div className="holiday">土、日&emsp;10:00〜20:00</div>
+        <div className="shopInfo">
+          <div className="mailAddress">
+            <div className="mail">841-0041</div>
+            <div className="address">佐賀県鳥栖市高田町73-4</div>
+          </div>
+          <div className="businessHours">
+            <div className="weekday">月〜金:10:00〜20:00</div>
+            <div className="holiday">土、日:10:00〜20:00</div>
+          </div>
         </div>
       </div>
       <div className="wrap">
